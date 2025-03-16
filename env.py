a7f2cf7ad82e0f9661d4ec79c028fb52cef73d8c
mrsamsonn <andrewsj.ahs@gmail.com>
2025-03-16 10:45:11 -0700

    obstacle detection

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ import pygame
 class buildEnvironment:
     def __init__(self, MapDimensions):
         pygame.init()
-        self.pointCould=[]
+        self.pointCloud=[]
         self.externalMap=pygame.image.load('map1.png')
         self.maph, self.mapw = MapDimensions
         self.MapWindowName = 'RRT path planning'
@@ -19,3 +19,21 @@ class buildEnvironment:
         self.green = (0,255,0)
         self.red = (255,0,0)
         self.white = (255,255,255)
+    
+    #helper function
+    def AD2pos(self, distance, angle, robotPosition):
+        x = distance * math.cos(angle)+robotPosition[0]
+        y = distance * math.sin(angle)+robotPosition[1]
+        return (int(x), int(y))
+    
+    def dataStorage(self, data):
+        print(len(self.pointCloud))
+        for element in data:
+            point = self.AD2pos(element[0], element[1], element[2])
+            if point not in self.pointCloud:
+                self.pointCloud.append(point)
+
+    def show_sensorData(self): #show data detected [red pixels]
+        self.infomap = self.map.copy()
+        for point in self.pointCloud:
+            self.infomap.set_at((int(point[0]), int(point[1])), (255, 0, 0))
